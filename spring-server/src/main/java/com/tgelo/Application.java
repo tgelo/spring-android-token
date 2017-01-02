60c0ec72a05e97cf887cbdd7fe65aab5e43f9a34
Tomek Gelo <tomekgelo@gmail.com>
2017-01-02 10:17:02 +0100

    Remove no more needed scheduling

--- spring-server/src/main/java/com/tgelo/Application.java
+++ spring-server/src/main/java/com/tgelo/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
